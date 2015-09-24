4ba7532585c7dc4565fe4df2026bad797b9fab93
kpcna <kpcna@hotmail.com>
2015-09-23 19:00:39 -0700

    Template

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,8 @@
-    <em>&copy; 2015</em>
+      <footer class="footer">
+        <p>&copy; Company 2014</p>
+      </footer>
+
+    </div> <!-- /container -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
         </body>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,11 +1,19 @@
-<?php
-$this->load->helper('url');
-?>
 <html>
         <head>
                 <title>CodeIgniter Tutorial</title>
 		<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+		<link href="<?php echo base_url("assets/css/jumbotron-narrow.css"); ?>" rel="stylesheet">
         </head>
         <body>
-
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Project name</h3>
+      </div>
                 <h1><?php echo $title; ?></h1>
