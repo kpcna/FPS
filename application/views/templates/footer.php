d85ddbea35ac5a41e841921ca0e244157ab3b8a7
kpcna <kpcna@hotmail.com>
2015-09-23 18:49:34 -0700

    Bootstrap

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,5 @@
     <em>&copy; 2015</em>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
         </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,7 @@
 <html>
         <head>
                 <title>CodeIgniter Tutorial</title>
+		<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
         </head>
         <body>
 
