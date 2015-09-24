814b442737a4637862efe3664fef20f753bd15be
kpcna <kpcna@hotmail.com>
2015-09-23 18:24:58 -0700

    Change base url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://45.55.59.184/';
+$config['base_url'] = 'http://45.55.59.184/FPS/';
 
 /*
 |--------------------------------------------------------------------------
