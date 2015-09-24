b78fd7d9fba1cb2b3334cd057d6878649660c345
kpcna <kpcna@hotmail.com>
2015-09-23 18:11:10 -0700

    Controller

---
+++ application/views/news/create.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>News - Create Controller</title>
+</head>
+<body>
+<h2><?php echo $title; ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('news/create'); ?>
+
+    <label for="title">Title</label>
+    <input type="input" name="title" /><br />
+
+    <label for="text">Text</label>
+    <textarea name="text"></textarea><br />
+
+    <input type="submit" name="submit" value="Create news item" />
+
+</form>
+</body>
+</html>
